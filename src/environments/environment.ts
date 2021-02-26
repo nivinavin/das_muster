85240841023ac5f2abfdacd533648244619cb12b
nivinavin <navinlovema30@gmail.com>
2021-02-26 11:48:59 +0530

    config file removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   config : {
-    apiKey: "AIzaSyBCJUUfo2xyoq-z0jZIidSFMY-yBO2qy3M",
-    authDomain: "hack-dasmuster.firebaseapp.com",
-    projectId: "hack-dasmuster",
-    storageBucket: "hack-dasmuster.appspot.com",
-    messagingSenderId: "744131576406",
-    appId: "1:744131576406:web:03a4344943c556f7abf1f7",
-    measurementId: "G-C0E05D21WG",
-    databaseURL: "https://hack-dasmuster-default-rtdb.firebaseio.com"
+    
   }
 };
 
